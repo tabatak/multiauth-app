148af3d874b6fc6d53c9c98f6f6dbcb0fa81aae3
tabatak <hamamoto.kazuto@gmail.com>
2022-04-16 21:01:24 +0900

    breeze導入
    これは利用する方針にしたい。とりあえずSPA系の機能はいったんなしにしたい

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+});
 
-require 'admin.php';
-require 'user.php';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
